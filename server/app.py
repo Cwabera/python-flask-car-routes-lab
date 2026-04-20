472fd1f780f90e3a26c452cc8dc2379d0b8d57fc
Charles <charlesngatia.cn@gmail.com>
2026-04-20 14:20:37 +0300

    .....

--- server/app.py
+++ server/app.py
@@ -4,18 +4,15 @@ app = Flask(__name__)
 
 models = ["crossroads", "roadster", "summit", "voyager"]
 
-
 @app.route("/")
 def index():
     return "Welcome to Flatiron Cars"
 
-
 @app.route("/car/<string:model_name>")
-def show_model(model_name):
+def model(model_name):
     if model_name.lower() in models:
         return f"Flatiron {model_name.title()} is in our fleet!"
     return f"No models called {model_name} exists in our catalog"
-
 
 if __name__ == "__main__":
     app.run(debug=True)
